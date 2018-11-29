a2e36a2a1b4085fcf720551c4814c0e672f62a23
Brian Kunze <bkunze@familysearch.org>
2018-11-28 17:13:04 -0700

    Initial results after interview.

--- src/main/java/com/github/markboydcode/questions/KaprekarsConstant.java
+++ src/main/java/com/github/markboydcode/questions/KaprekarsConstant.java
@@ -1,5 +1,8 @@
 package com.github.markboydcode.questions;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Calculates for any four digit number the number of iterations it takes to arrive at Kaprekar's constant of 6174 by
  * the following steps:
@@ -17,7 +20,14 @@ public class KaprekarsConstant {
   static final String TOO_HIGH = "the value is greater than 9999";
   static final String NO_VARIANCE = "the value contains digits that are all identical";
 
+  private static final String FOUR_DIGIT_FORMAT =  "%04d";
+  private static final int DIGITS =  4;
+
   private static final int THE_CONSTANT = 6174;
+
+
+  public KaprekarsConstant() {
+  }
 
   /**
    *
@@ -33,8 +43,44 @@ public class KaprekarsConstant {
       throw new IllegalArgumentException(TOO_HIGH);
     }
 
-    // your implementation here
+      int count = 0;
 
-    return 0;
+      while(true){
+        count++;
+        String s =  String.format(FOUR_DIGIT_FORMAT, fourDigitNumber);
+
+        Integer[] values  = new Integer[4];
+
+        for(int j = 0; j < s.length(); j++){
+          String c = s.substring(j,j+1);
+          values[j] = Integer.valueOf(c);
+        }
+
+        Arrays.sort(values);
+
+        String sLowest = Arrays.stream(values)
+                .map(i -> String.valueOf(i))
+                .collect(Collectors.joining());
+
+        int low = padLeadingZeros(sLowest);
+        int high = padLeadingZeros(new StringBuilder(sLowest).reverse().toString());
+
+        fourDigitNumber = high - low;
+
+        if(fourDigitNumber == THE_CONSTANT)
+          return count;
+
+      }
+
+
   }
+
+  private int padLeadingZeros(String value) {
+    String result = value;
+    if(value != null && value.length() < DIGITS){
+       result = String.format(FOUR_DIGIT_FORMAT, Integer.valueOf(value));
+    }
+    return Integer.valueOf(result);
+  }
+
 }
